379b44bb4d9de1f37e3ba37876f63982fc6015fe
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-09-10 20:54:05 +0530

    chore: add Tailwind CSS and PostCSS dependencies to package.json

--- frontend/sihara-constructions/src/App.jsx
+++ frontend/sihara-constructions/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+// Pages (match your folder structure)
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Services from "./pages/services/Services";
+import Projects from "./pages/projects/Project";
+import Contact from "./pages/contact/ContactPage";
+import Navbar from "./components/navbar";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
