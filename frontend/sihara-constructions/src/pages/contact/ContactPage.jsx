7b3eb1afaa47eec00f5f1cfcc35452be6968856e
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2025-09-18 21:27:48 +0530

    setup hero section container

--- frontend/sihara-constructions/src/pages/contact/ContactPage.jsx
+++ frontend/sihara-constructions/src/pages/contact/ContactPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import heroImage from '../../assets/images/contact-hero.jpg'
+import { useState } from "react";
+import heroImage from "../../assets/contact-hero1.jpg";
 
-const ContactPage = () => {
+export default function ContactPage() {
   return (
-    <div>
+    <div className="flex flex-col">
       
+      <section
+        className="relative bg-cover bg-center text-white h-[100vh] px-6 text-center flex flex-col justify-center"
+        style={{
+          backgroundImage: `url(${heroImage})`,
+          clipPath: "ellipse(100% 85% at 50% 0%)",
+        }}
+      >
+        
+      </section>
     </div>
-  )
+  );
 }
-
-export default ContactPage
